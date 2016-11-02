7157d28f78bbc76ea2e15fadd6927873f4358498
Ivandro Ismael <ivandrofly@gmail.com>
2016-11-02 19:28:33 +0000

    [Factory method] - Add implementation of factory method.

---
+++ Factory Method/Program.RealWorldCode.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// Creates an instance of several derived classes.
+/// 
+/// Define an interface for creating an object, but let subclasses decide which class to instantiate.
+/// Factory Method lets a class defer instantiation to subclasses.
+/// 
+/// Frequency of use: 5 high.
+/// </summary>
+namespace DoFactory.GangOfFour.Factory.RealWorld
+{
+    /// <summary>
+    /// MainApp startup class for Real-World 
+    /// Factory Method Design Pattern.
+    /// </summary>
+    class MainApp
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
+        {
+            // Note: constructors call Factory Method
+            Document[] documents = new Document[2];
+
+            documents[0] = new Resume();
+            documents[1] = new Report();
+
+            // Display document pages
+            foreach (Document document in documents)
+            {
+                Console.WriteLine("\n" + document.GetType().Name + "--");
+                foreach (Page page in document.Pages)
+                {
+                    Console.WriteLine(" " + page.GetType().Name);
+                }
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Product' abstract class
+    /// </summary>
+    abstract class Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class SkillsPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class EducationPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class ExperiencePage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class IntroductionPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class ResultsPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class ConclusionPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class SummaryPage : Page
+    {
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    class BibliographyPage : Page
+    {
+    }
+
+    /// <summary>
+    /// The 'Creator' abstract class
+    /// </summary>
+    abstract class Document
+    {
+        private List<Page> _pages = new List<Page>();
+
+        // Constructor calls abstract Factory method
+        public Document()
+        {
+            this.CreatePages();
+        }
+
+        public List<Page> Pages
+        {
+            get { return _pages; }
+        }
+
+        // Factory Method
+        public abstract void CreatePages();
+    }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    class Resume : Document
+    {
+        // Factory Method implementation
+        public override void CreatePages()
+        {
+            Pages.Add(new SkillsPage());
+            Pages.Add(new EducationPage());
+            Pages.Add(new ExperiencePage());
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    class Report : Document
+    {
+        // Factory Method implementation
+        public override void CreatePages()
+        {
+            Pages.Add(new IntroductionPage());
+            Pages.Add(new ResultsPage());
+            Pages.Add(new ConclusionPage());
+            Pages.Add(new SummaryPage());
+            Pages.Add(new BibliographyPage());
+        }
+    }
+}
+
+// Output:
+//Resume -------
+// SkillsPage
+// EducationPage
+// ExperiencePage
+
+//Report -------
+// IntroductionPage
+// ResultsPage
+// ConclusionPage
+// SummaryPage
+// BibliographyPage
